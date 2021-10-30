e97e006461580e48a5f37d1dd7899f8330450ad3
Daniel Kim <danielkim2711@gmail.com>
2021-10-30 19:13:12 +1300

    Fetch and display data from firebase

--- app/src/main/java/cs/daniel/mobileassignmenttwo/MainActivity.java
+++ app/src/main/java/cs/daniel/mobileassignmenttwo/MainActivity.java
@@ -6,9 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    MainAdapter mainAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = (RecyclerView)findViewById(R.id.rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<Employee> options = new FirebaseRecyclerOptions.Builder<Employee>()
+                .setQuery(FirebaseDatabase.getInstance().getReference().child("employees"), Employee.class)
+                .build();
+
+        mainAdapter = new MainAdapter(options);
+        recyclerView.setAdapter(mainAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mainAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mainAdapter.stopListening();
     }
 }
