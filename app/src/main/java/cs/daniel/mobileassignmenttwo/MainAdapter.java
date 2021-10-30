2e28cbd45ace4997d538d61e6db54efdbdfa924e
Daniel Kim <danielkim2711@gmail.com>
2021-10-30 22:04:42 +1300

    Define update pannel

--- app/src/main/java/cs/daniel/mobileassignmenttwo/MainAdapter.java
+++ app/src/main/java/cs/daniel/mobileassignmenttwo/MainAdapter.java
@@ -3,6 +3,7 @@ package cs.daniel.mobileassignmenttwo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
 
 public class MainAdapter extends FirebaseRecyclerAdapter<Employee,MainAdapter.myViewHolder> {
 
@@ -23,13 +26,31 @@ public class MainAdapter extends FirebaseRecyclerAdapter<Employee,MainAdapter.my
         super(options);
     }
 
+    // Bind data using class
+
     @Override
     protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull Employee model) {
         holder.country.setText(model.getCountry());
         holder.email.setText(model.getEmail());
         holder.name.setText(model.getName());
         holder.position.setText(model.getPosition());
+
+    // Set onClickListener for update button
+
+        holder.btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.name.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.update_popup))
+                        .setExpanded(true, 1200)
+                        .create();
+
+                 dialogPlus.show();
+            }
+        });
     }
+
+    // Define IDs
 
     @NonNull
     @Override
@@ -41,6 +62,7 @@ public class MainAdapter extends FirebaseRecyclerAdapter<Employee,MainAdapter.my
     class myViewHolder extends RecyclerView.ViewHolder {
 
         TextView country, email, name, position;
+        Button btnUpdate, btnDelete;
 
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -49,6 +71,9 @@ public class MainAdapter extends FirebaseRecyclerAdapter<Employee,MainAdapter.my
             email = (TextView)itemView.findViewById(R.id.tvEmail);
             name = (TextView)itemView.findViewById(R.id.tvName);
             position = (TextView)itemView.findViewById(R.id.tvPosition);
+
+            btnUpdate = (Button)itemView.findViewById(R.id.btnUpdate);
+            btnDelete = (Button)itemView.findViewById(R.id.btnDelete);
         }
     }
 }
