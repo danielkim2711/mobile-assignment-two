408a0d2290bbb2c62b936f02a1827c69360663b0
Daniel Kim <danielkim2711@gmail.com>
2021-10-31 17:13:59 +1300

    Define add pannel

--- app/src/main/java/cs/daniel/mobileassignmenttwo/MainActivity.java
+++ app/src/main/java/cs/daniel/mobileassignmenttwo/MainActivity.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.SearchView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     MainAdapter mainAdapter;
+
+    FloatingActionButton floatingActionButton;
 
     // Fetch data and display from Firebase
 
@@ -33,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
 
         mainAdapter = new MainAdapter(options);
         recyclerView.setAdapter(mainAdapter);
+
+        // Set onClickListener for add button
+
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),AddActivity.class));
+            }
+        });
     }
 
     @Override
